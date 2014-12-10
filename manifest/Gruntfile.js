d432a6d410e0433b165f753796f7886688a7d07c
psychobunny <psycho.bunny@hotmail.com>
2014-12-10 16:09:40 -0500

    manifest init

---
+++ manifest/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		express: {
+			dev: {
+				options: {
+					script: 'server.js'
+				}
+			},
+		},
+		less: {
+			development: {
+				files: {
+					"public/bin/style.css": "source/style.less"
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: "source/**/*.less",
+				tasks: ['less'],
+				options: {
+					livereload: 3001,
+				}
+			},
+			html: {
+				files: ["public/**/*.html", "public/**/*.js"],
+				options: {
+					livereload: 3001
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-express-server');
+
+	grunt.registerTask('default', ['express:dev', 'less', 'watch']);
+};
---
+++ manifest/README.md
@@ -0,0 +1,19 @@
+## Installation 
+
+First make sure you've installed [git](http://git-scm.com/downloads), [node.js](http://nodejs.org/download/), and [grunt-cli](http://gruntjs.com/getting-started).
+
+```
+git clone https://github.com/psychobunny/Manifest.git
+cd Manifest
+npm install
+```
+
+## Using Manifest
+
+To boot up Manifest, simply run the following statement.
+
+```
+grunt
+```
+
+You should get a welcome screen when you navigate to http://localhost:3000/
