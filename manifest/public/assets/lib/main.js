94fbb6b76e4520be882cb34233f14ae63fb5f139
psychobunny <psycho.bunny@hotmail.com>
2015-03-05 16:42:47 -0500

    brand new hero animation :+1:

--- manifest/public/assets/lib/main.js
+++ manifest/public/assets/lib/main.js
@@ -24,7 +24,64 @@ $(document).ready(function() {
 
 		configureMobilePricing();
 	});
+
+	setupCanvas();
 });
+
+
+var frames = 15;
+var durations = [
+	500,
+	3300,
+	3700,
+	4000,
+	4100,
+	4400,
+	4700,
+	4750,
+	4775,
+	4820,
+	4910,
+	5050,
+	5051,
+	5080,
+	5120
+]
+
+function setupCanvas() {
+	loadImages(function() {
+		for (var i = 1; i <= frames; i++) {
+			(function(i) {
+				setTimeout(function() {
+					$('.frame' + i).addClass('phaseIn rubberBand animated');
+					if (i === 1) {
+						$('.hero-signup span').addClass('phaseIn');
+					} else if (i === frames) {
+						$('.hero-signup a').addClass('phaseIn');
+					}
+				}, durations[i - 1] / 1.5);
+			}(i));
+		}
+	});
+}
+
+function loadImages(cb) {
+	var loaded = 0;
+	function loadFinish() {
+		loaded++;
+		if (loaded === frames) {
+			cb();
+		}
+	}
+
+	for (var i = 1; i <= frames; i++) {
+		var img = document.createElement('img');
+		img.onload = loadFinish;
+		img.src = 'assets/animation/frames/frame' + i + '.gif';
+		img.className = 'frame frame' + i;
+		$('#canvas').append(img);
+	}
+}
 
 
 function resizeHeader() {
