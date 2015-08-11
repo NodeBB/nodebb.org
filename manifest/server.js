57cefc10d00392d0622cc2368417925862f67eb3
psychobunny <rodrigues.andrew@gmail.com>
2015-08-11 01:26:41 -0400

    a really simple router/engine to make life easier

--- manifest/server.js
+++ manifest/server.js
@@ -2,7 +2,9 @@
 
 var express = require('express'),
 	winston = require('winston'),
-	app = express();
+	async = require('async'),
+	app = express(),
+	filecache = {};
 
 winston.remove(winston.transports.Console);
 winston.add(winston.transports.Console, {
@@ -11,9 +13,43 @@ winston.add(winston.transports.Console, {
 
 app.use(express.static('public', {}));
 
+app.get('/:page?', function (req, res) {
+	renderPage(req.params.page || 'index', function(err, page) {
+		if (err) {
+			return res.status(404);
+		}
+
+		res.send(filecache['header'] + page + filecache['footer']);
+	});
+});
+
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	winston.info('Manifest is ready and listening on http://%s:%s', host, port);
+	async.each(['header', 'footer'], renderPage, function() {
+		winston.info('Manifest is ready and listening on http://%s:%s', host, port);
+	});
 });
+
+
+
+function renderPage(page, callback) {
+	if (filecache[page]) {
+		return callback(false, filecache[page]);
+	}
+
+	var fs = require('fs'),
+		path = require('path');
+
+	fs.readFile(path.join(__dirname, 'templates', page + '.html'), function(err, file) {
+		if (err || !file) {
+			return callback(true);
+		}
+
+		file = file.toString();
+		filecache[page] = file;
+		
+		return callback(false, file);
+	});
+}
