2e3c07a3536ccc628450badbd017560aba50b0ef
trevor <trevor@engineer.com>
2020-07-10 23:06:18 -0500

    contact form adjustments

--- src/footer-contact.vue
+++ src/footer-contact.vue
@@ -226,6 +226,15 @@ export default {
   .el-modal-pad {
     padding: 3rem 0 0.25rem !important;
     border-radius: 0.5rem !important;
+
+    @media (max-width: 767px) {
+      border-radius: 0 !important;
+      padding-top: 0 !important;
+    }
+  }
+  .close i {
+    font-family: Font Awesome 5 Pro;
+    font-weight: 300;
   }
   .el-modal-content {
     border-radius: 0;
@@ -234,19 +243,17 @@ export default {
     width: 100% !important;
     max-width: 620px !important;
     min-height: 100vh !important;
+    .modal-text {
+      .modal-text-content {
+        flex-basis: 100%;
+        text-align: center;
+        max-width: 100%;
+        overflow: hidden;
+        padding-top: 56.25%;
+        position: relative;
+      }
+    }
   }
-  .modal-text-content {
-    flex-basis: 100%;
-    text-align: center;
-    max-width: 100%;
-    overflow: hidden;
-    padding-top: 56.25%;
-    position: relative;
-  }
-}
-.close i {
-  font-family: Font Awesome 5 Pro;
-  font-weight: 300;
 }
 
 iframe {
